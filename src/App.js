03373d87ee7bd5711530552c9d7aca8e93239614
Syed Ahamed <salearning01@gmail.com>
2025-05-08 06:58:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axiosInstance from "./axios/axiosInstance";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const getUserList = () => {
+    axiosInstance
+      .get("/posts")
+      .then((result) => {
+        setUserList(result);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+  };
+  useEffect(() => {
+    getUserList();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        <ul>
+          {userList?.map((item) => {
+            return <li>{item?.id}</li>;
+          })}
+        </ul>
+      }
+    </>
   );
 }
 
---
+++ src/axios/axiosInstance.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const token = "eyyeyyeyyeyyeyyeyyeyyeyyeyyeyyeyyeyy";
+const axiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/",
+});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("Config", config);
+    config.headers.Authorization = `Bearer ${token}`;
+    console.log("Config 2", config);
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("response", response);
+    return response?.data;
+  },
+  (error) => Promise.reject(error)
+);
+export default axiosInstance;
